53cdf1df80722136996a1e8d5edae463294feb25
Jules Maffre <julien.maffre@wayve.ai>
2023-10-07 14:19:04 +0100

    working

--- minigrep/src/lib.rs
+++ minigrep/src/lib.rs
@@ -1,3 +1,4 @@
+use std::env;
 use std::error::Error;
 use std::fs;
 
@@ -5,26 +6,72 @@ use std::fs;
 pub struct Config {
     pub query: String,
     pub file_path: String,
+    pub ignore_case: bool,
 }
 
 impl Config {
     pub fn build(args: &Vec<String>) -> Result<Config, &'static str> {
         if args.len() != 3 {
-            // panic!("Error: usage {} <query> <file>", args[0]);
             return Err("function takes 3 arguments");
         }
 
         let query = args[1].clone();
         let file_path = args[2].clone();
 
-        Ok(Config { query, file_path })
+        let ignore_case = env::var("IGNORE_CASE").is_ok();
+
+        Ok(Config {
+            query,
+            file_path,
+            ignore_case,
+        })
     }
 }
 
 pub fn run(config: &Config) -> Result<(), Box<dyn Error>> {
     let content = fs::read_to_string(&config.file_path)?;
 
-    println!("File content: {}", content);
+    for line in search(&config.query, &content, !config.ignore_case) {
+        println!("{line}");
+    }
 
     Ok(())
+}
+
+pub fn search<'a>(query: &str, content: &'a str, case_sensitive: bool) -> Vec<&'a str> {
+    let mut results = Vec::new();
+    let query = if case_sensitive {
+        query.to_string()
+    } else {
+        query.to_lowercase()
+    };
+    println!("Case sensitive: {case_sensitive}");
+    for line in content.lines() {
+        if (case_sensitive && line.contains(&query))
+            || (!case_sensitive && line.to_lowercase().contains(&query))
+        {
+            results.push(line);
+        }
+    }
+    results
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    #[test]
+    fn simple_case_sensitive() {
+        let content = "lili\nlala\ntest\nLILI";
+        assert_eq!(vec!["lili"], search("li", content, true));
+        assert_eq!(vec!["LILI"], search("LI", content, true));
+        assert!(search("Li", content, true).is_empty());
+    }
+
+    #[test]
+    fn simple_case_insensitive() {
+        let content = "lili\nlala\ntest\nLIli";
+        assert_eq!(vec!["lili", "LIli"], search("li", content, false));
+        assert_eq!(vec!["lili", "LIli"], search("Li", content, false));
+        assert_eq!(vec!["lili", "LIli"], search("LI", content, false));
+    }
 }
--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -7,19 +7,16 @@ fn main() {
     let args: Vec<String> = env::args().collect();
 
     let config = Config::build(&args).unwrap_or_else(|err| {
-        println!(
+        eprintln!(
             "Error parsing arguments: usage {} <query> <file_path>: {}",
             args[0], err
         );
         process::exit(1);
     });
 
-    println!("Configuration: {:?}", config);
-    println!("Query: {}", config.query);
-
     // Note: unwrap_or_else() would work too but because returns () on success, we don't need to unwrap the result
     if let Err(e) = minigrep::run(&config) {
-        println!("Application error: {}", e);
+        eprintln!("Application error: {}", e);
         process::exit(1);
     }
 }
