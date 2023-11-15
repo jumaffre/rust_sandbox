d0a41d2a9e401bc8d599034d2845d4febeaff6ef
Jules Maffre <julien.maffre@wayve.ai>
2023-11-15 14:11:06 +0000

    This works!

--- config_libs/lib_a/src/lib.rs
+++ config_libs/lib_a/src/lib.rs
@@ -1,7 +1,11 @@
-// Here, lib_a depends on the specific type of lib_b, only available when the
-// "experimental" feature isn't enabled.
+// Here, lib_a depends on lib_b, only available when the
+// "experimental" feature is _not_ enabled.
 
 #[allow(dead_code)]
 pub struct AType {
+    #[cfg(not(feature = "experimental_a"))]
     pub b_type: lib_b::BType,
+
+    #[cfg(feature = "experimental_a")]
+    pub b_type: lib_b::BTypeExperimental,
 }
